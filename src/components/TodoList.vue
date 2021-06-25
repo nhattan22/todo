c3a812bef83606211d83beb1d02d6e41b95a5b2f
USER <n15dcpt017@student.ptithcm.edu.vn>
2021-06-25 12:31:29 +0700

    init todo

---
+++ src/components/TodoList.vue
@@ -0,0 +1,216 @@
+<template>
+  <div>
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+      integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+      crossorigin="anonymous"
+    />
+    <input type="text" class="todo-input" placeholder="Nhập nội dung" v-model="newTodo" @keyup.enter="addTodo">
+    <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
+    <div v-for="(todo, index) in todosFiltered" :key="todo.id" class="todo-item">
+      <div class="todo-item-left">
+        <input type="checkbox" v-model="todo.completed">
+        <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" :class="{ completed : todo.completed }">{{ todo.title }}</div>
+        <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
+      </div>
+      <div class="remove-item" @click="removeTodo(index)">
+        <i class="fas fa-times"></i>
+      </div>
+    </div>
+    </transition-group>
+
+    <div class="extra-container">
+      <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos"> Check All</label></div>
+      <div>{{ remaining }} items left</div>
+    </div>
+
+    <div class="extra-container">
+      <div>
+        <button :class="{ active: filter == 'all' }" @click="filter = 'all'">All</button>
+        <button :class="{ active: filter == 'active' }" @click="filter = 'active'">Active</button>
+        <button :class="{ active: filter == 'completed' }" @click="filter = 'completed'">Completed</button>
+      </div>
+
+      <div>
+        <transition name="fade">
+        <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+        </transition>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo-list",
+  data() {
+    return {
+        filter: 'all',
+      beforeEdit: "",
+      idTodo: 3,
+      newTodo: "",
+      todos: [
+        {
+          id: 1,
+          title: "Đi chơi",
+          comleted: false,
+          editing: false,
+        },
+        {
+          id: 2,
+          title: "Đi chợ",
+          comleted: false,
+          editing: false,
+        },
+      ],
+    };
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus();
+      },
+    },
+  },
+  computed: {
+      remaining() {
+          return this.todos.filter(todo => !todo.completed).length
+      },
+      anyRemaining() {
+          return this.remaining !=0
+      },
+          todosFiltered() {
+      if (this.filter == 'all') {
+        return this.todos
+      } else if (this.filter == 'active') {
+        return this.todos.filter(todo => !todo.completed)
+      } else if (this.filter == 'completed') {
+        return this.todos.filter(todo => todo.completed)
+      }
+
+      return this.todos
+    },
+      showClearCompletedButton() {
+      return this.todos.filter(todo => todo.completed).length > 0
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim().length === 0) {
+        return;
+      }
+      this.todos.push({
+        id: this.idTodo,
+        title: this.newTodo,
+        comleted: false,
+      });
+      this.newTodo = "";
+      this.idTodo++;
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      this.beforeEdit = todo.title;
+      todo.editing = true;
+    },
+    doneEdit(todo) {
+      if (todo.title.trim() === "") {
+        todo.title = this.beforeEdit;
+      }
+      todo.editing = false;
+    },
+    cancelEdit(todo) {
+      todo.title = this.beforeEdit;
+      todo.editing = false;
+    },
+    checkAllTodos() {
+      this.todos.forEach((todo) => todo.completed = event.target.checked)
+    },
+      clearCompleted() {
+      this.todos = this.todos.filter(todo => !todo.completed)
+    }
+  },
+};
+</script>
+
+
+<style>
+
+@import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+}
+.todo-input:focus {
+  outline: 0;
+}
+.todo-item {
+  margin-bottom: 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  animation-duration: 0.3s;
+}
+.remove-item {
+  cursor: pointer;
+  margin-left: 14px;
+}
+.remove-item:hover {
+  color: black;
+}
+.todo-item-left {
+  display: flex;
+  align-items: center;
+}
+.todo-item-edit {
+  font-size: 24px;
+  color: #2c3e50;
+  margin-left: 12px;
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+}
+.todo-item-edit:focus {
+  outline: none;
+}
+.completed {
+  text-decoration: line-through;
+  color: grey;
+}
+.extra-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgrey;
+    padding-top: 14px;
+    margin-bottom: 14px;
+  }
+  button {
+    font-size: 14px;
+    background-color: white;
+    appearance: none;
+  }
+  button:hover {
+      background: lightgreen;
+  }
+  .button:focus {
+      outline: none;
+  }
+  .active {
+    background: lightgreen;
+  }
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
+</style>
